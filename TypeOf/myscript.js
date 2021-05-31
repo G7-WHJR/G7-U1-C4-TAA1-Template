3e50e24b9cf6356e0bd0d4f97dd2a5c92011a0cb
G7-WHJR <raveen.anand@whitehatjr.com>
2021-05-31 22:49:18 +0530

    First

---
+++ TypeOf/myscript.js
@@ -0,0 +1,12 @@
+function check()
+{
+    // 1. Declare a variable x, with value 5
+    
+
+    // 2. Declare a variable y, with value "Coding"
+    
+
+   // 3. Check type of the variables using typeOf operator , print n console
+   
+}
+
